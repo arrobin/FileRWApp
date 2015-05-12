60835108b0da609cff685b825d5763fd688baf4c
arrobin <ashiqur007@yahoo.com>
2015-05-12 19:16:09 +0600

    FileRWApp

---
+++ FileRWApp/FileRWUi.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FileRWApp
+{
+    partial class FileRWUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.showListBox = new System.Windows.Forms.ListBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(343, 53);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(117, 53);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(207, 20);
+            this.firstNameTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(117, 92);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(207, 20);
+            this.lastNameTextBox.TabIndex = 2;
+            // 
+            // showListBox
+            // 
+            this.showListBox.FormattingEnabled = true;
+            this.showListBox.Location = new System.Drawing.Point(38, 134);
+            this.showListBox.Name = "showListBox";
+            this.showListBox.Size = new System.Drawing.Size(369, 134);
+            this.showListBox.TabIndex = 3;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(343, 89);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // FileRWUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 307);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.showListBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "FileRWUi";
+            this.Text = "FileRWUi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.ListBox showListBox;
+        private System.Windows.Forms.Button showButton;
+    }
+}
+
---
+++ FileRWApp/FileRWUi.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileRWApp
+{
+    public partial class FileRWUi : Form
+    {
+        string path = @"E:\.NET Web App(Batch-4)\02-05-2015\FileRWApp\info.text";
+        public FileRWUi()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            
+            FileStream aFileStream = new FileStream(path,FileMode.Append);
+            StreamWriter aStreamWriter = new StreamWriter(aFileStream);
+
+            aStreamWriter.Write("Name:");
+            aStreamWriter.Write(firstNameTextBox.Text);
+            aStreamWriter.Write(" ");
+            aStreamWriter.WriteLine(lastNameTextBox.Text);
+
+            aStreamWriter.Close();
+            aFileStream.Close();
+            firstNameTextBox.Clear();
+            lastNameTextBox.Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            showListBox.Items.Clear();
+            FileStream aFileStream = new FileStream(path, FileMode.Open);
+            StreamReader aStreamReader = new StreamReader(aFileStream);
+
+            while (!aStreamReader.EndOfStream)
+            {
+                string name = aStreamReader.ReadLine();
+                showListBox.Items.Add(name);
+            }
+            aStreamReader.Close();
+            aFileStream.Close();
+        }
+    }
+}
